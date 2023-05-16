ec0d364501a7a389abe315d86f3a651bf9a8b7ab
Fernanda Caramico <46379117+fernandacaramico@users.noreply.github.com>
2023-05-15 21:46:14 -0300

    Atualiza comentário de Banco de Dados MySQL Workbench

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -55,7 +55,7 @@ function executar(instrucao) {
                 resolve(resultados);
             });
             conexao.on('error', function (erro) {
-                return ("ERRO NO MySQL WORKBENCH (Local): ", erro.sqlMessage);
+                return ("ERRO NO MySQL WORKBENCH: ", erro.sqlMessage);
             });
         });
     } else {
