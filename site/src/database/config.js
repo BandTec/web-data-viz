4fbf95a685088ff79ff0f9e33b4eb3ff8a0ef57c
Fernanda Caramico <46379117+fernandacaramico@users.noreply.github.com>
2023-11-07 10:15:13 -0300

    Atualiza configs de bd

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH
 var mySqlConfig = {
     host: "localhost",
-    database: "aquatech",
-    user: "root",
-    password: "william",
+    database: "SEU_BANCO_DE_DADOS",
+    user: "SEU_USUARIO",
+    password: "SUA_SENHA",
 };
 
 function executar(instrucao) {
