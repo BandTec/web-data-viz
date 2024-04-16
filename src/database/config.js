0ec4e873f477ebff6a2d6cbde6ca627fad317d3f
fernandacaramico <fernanda.caramico@sptech.school>
2024-04-16 18:35:14 -0300

    melhoria - instrução em readme

--- src/database/config.js
+++ src/database/config.js
@@ -12,7 +12,7 @@ var mySqlConfig = {
 function executar(instrucao) {
 
     if (process.env.AMBIENTE_PROCESSO !== "producao" && process.env.AMBIENTE_PROCESSO !== "desenvolvimento") {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM .env\n");
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM .env OU dev.env OU app.js\n");
         return Promise.reject("AMBIENTE NÃO CONFIGURADO EM .env");
     }
 
