1b53fead9c349f8872a0c9bd2389c0cfb5b88bef
Joaovictordm <joao.rodrigues@sptech.school>
2026-06-02 11:33:04 -0300

    fix:adjusting the logic

--- public/dashboard/alertas/script.js
+++ public/dashboard/alertas/script.js
@@ -11,10 +11,10 @@ function msToTime(ms) {
   let minutes = (ms / (1000 * 60)).toFixed(0);
   let hours = (ms / (1000 * 60 * 60)).toFixed(0);
   let days = (ms / (1000 * 60 * 60 * 24)).toFixed(0);
-  if (seconds < 60) return seconds + " Segundos";
-  else if (minutes < 60) return minutes + " Minutos";
-  else if (hours < 24) return hours + " Horas";
-  else return days + " Dias"
+  if (seconds < 60) return seconds + " Segundo" + (seconds > 1 ? "s":"");
+  else if (minutes < 60) return minutes + " Minuto" + (minutes > 1 ? "s":"");
+  else if (hours < 24) return hours + " Hora" + (hours > 1 ? "s":"")
+  else return days + " Dia" + (days > 1 ? "s":"")
 }
 
 
@@ -28,29 +28,36 @@ function callGetAlertsFromProdutor() {
   }).then((res) => {
     res.json().then((response) => {
       for (let i = 0; i < response.length; i++) {
-        let hora = response[i].enviado_em[0] + response[0].enviado_em[1];
-        let minuto = response[i].enviado_em[3] + response[0].enviado_em[4];
-        let segundo = response[i].enviado_em[6] + response[0].enviado_em[7];
-        
-        let horaAtual = new Date().getHours();
-        console.log(horaAtual, "hora atual")
-        let minutoAtual = new Date().getMinutes();
-        console.log(horaAtual, "hora atual")
-        let segundoAtual = new Date().getSeconds();
-        console.log(horaAtual, "hora atual")
+        let dataEnviado = new Date(response[i].enviado_em);
+        let dataAtual = new Date();
+        let diferencaMili = Math.abs(dataAtual.getTime() - dataEnviado.getTime())
+        let tempo = msToTime(diferencaMili)
 
-        let milisegundoAtual = (Number(horaAtual) * 3600000) + (Number(minutoAtual) * 60000) + (Number(segundoAtual) * 1000)
-        let milisegundoAlerta = (Number(hora) * 3_600_000) + (Number(minuto) * 60_000) + (Number(segundo) * 1000);
-        console.log(milisegundoAlerta, "emitido")
-        console.log(milisegundoAtual, "atual")
-        let tempo = msToTime((milisegundoAtual - milisegundoAlerta));
+        // let hora = response[i].enviado_em[0] + response[0].enviado_em[1];
+        // let minuto = response[i].enviado_em[3] + response[0].enviado_em[4];
+        // let segundo = response[i].enviado_em[6] + response[0].enviado_em[7];
+
+        
+        
+        // let horaAtual = new Date().getHours();
+        // console.log(horaAtual, "hora atual")
+        // let minutoAtual = new Date().getMinutes();
+        // console.log(horaAtual, "hora atual")
+        // let segundoAtual = new Date().getSeconds();
+        // console.log(horaAtual, "hora atual")
+
+        // let milisegundoAtual = (Number(horaAtual) * 3600000) + (Number(minutoAtual) * 60000) + (Number(segundoAtual) * 1000)
+        // let milisegundoAlerta = (Number(hora) * 3_600_000) + (Number(minuto) * 60_000) + (Number(segundo) * 1000);
+        // console.log(milisegundoAlerta, "emitido")
+        // console.log(milisegundoAtual, "atual")
+        // let tempo = msToTime((milisegundoAtual - milisegundoAlerta));
         if (response[i].prioridade == 0) {
           alertaSection.innerHTML += ` 
             <div class="alert normal">
               <div class="heading">
               <i class="ph-bold ph-info icon"></i>
               <h1 class="title"><strong>${response[i].tipo[0].toUpperCase() + response[i].tipo.substring(1)}</strong> na composteira ${response[i].modelo}</h1>
-              <p class="date">${tempo} Atrás</p>
+              <p class="date">${tempo} atrás</p>
             </div>
             <p class="desc">
                  ${response[i].descricao}
@@ -64,7 +71,7 @@ function callGetAlertsFromProdutor() {
             <div class="heading">
               <i class="ph-bold ph-warning-diamond icon"></i>
               <h1 class="title"><strong>${response[i].tipo[0].toUpperCase() + response[i].tipo.substring(1)}</strong> na composteira ${response[i].modelo}</h1>
-              <p class="date">${tempo} Atrás</p>
+              <p class="date">${tempo} atrás</p>
             </div>
               <p class="desc">
                 ${response[i].descricao}
@@ -78,7 +85,7 @@ function callGetAlertsFromProdutor() {
             <div class="heading">
               <i class="ph-bold ph-warning icon"></i>
               <h1 class="title"><strong>${response[i].tipo[0].toUpperCase() + response[i].tipo.substring(1)}</strong> na composteira ${response[i].modelo} </h1>
-              <p class="date">${tempo} Atrás</p>
+              <p class="date">${tempo} atrás</p>
             </div>
             <p class="desc">
              ${response[i].descricao}
@@ -92,7 +99,7 @@ function callGetAlertsFromProdutor() {
               <div class="heading">
                 <i class="ph-bold ph-warning-octagon icon"></i>
                 <h1 class="title"><strong>${response[i].tipo[0].toUpperCase() + response[i].tipo.substring(1)}</strong> na composteira ${response[i].modelo} </h1>
-                <p class="date">${tempo} Atrás</p>
+                <p class="date">${tempo} atrás</p>
               </div>
               <p class="desc">
                 ${response[i].descricao}
